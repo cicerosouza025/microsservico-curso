a6a1a3e417db984d4930569bd5e93af57cc122a4
Cicero de Souza <cicero025@live.com>
2021-05-16 20:36:46 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/cicero/hrpayroll/entities/Worker.java
@@ -0,0 +1,25 @@
+package com.cicero.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Worker implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+	
+	public Worker() {}
+
+	public Worker(Long id, String name, Double dailyIncome) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+}
